c29abe447b85a5e91391ce077fb54860364b1b5a
siyuanxu <i@siyuanxu.com>
2017-07-04 14:07:56 +0800

    dynamic plot finish

--- project_pot/plot_analog_value.py
+++ project_pot/plot_analog_value.py
@@ -3,20 +3,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+def dynamic_update_plot(line, x, y, new_x, new_y):
+    ax = plt.gca()
+    x = np.append(x, new_x)
+    y = np.append(y, new_y)
+    line.set_xdata(x)
+    line.set_ydata(y)
+    ax.relim()
+    ax.autoscale_view(True, True, True)
+    plt.draw()
+    plt.pause(1e-17)
+    return x, y
+
+
 plt.ion()
 # open serial port
 ser = serial.Serial(port='COM6', baudrate=9600)
-v = []
-pv = []
-plt.ion()
+
+pv = np.array([])
+x = np.array([])
+ax = plt.gca()
+
+line, = plt.plot(x, pv)
 i = 0
 while ser.isOpen():
-    val = ser.readline()
-    print(val)
-    # v.append(float(val))
-    # i += 1
-    # if i == 2:
-    #     pv.append(val)
-    #     i = 0
-    #     plt.plot(pv, '-r')
-    #     plt.pause(0.1)
+
+    i += 1
+
+    val = int(ser.readline())
+
+    x, pv = dynamic_update_plot(line, x, pv, i, val)
